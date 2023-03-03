c0a773f92c78081425f29f5e30c376c6e25d088d
Vishal Dubey <92910196+Vishal1732@users.noreply.github.com>
2023-03-04 01:53:39 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,16 +1,23 @@
-<!----- Shape.js ----->
 // declare class
-class Shape {
-     constructor() {
-          this.color = "green";
-     }
-     drawShape() {
+class Shape{
+    constructor(name){
+       this.name;
 
-     }
-     calculateArea() {
+    }
 
-     }
+    drawShape(){
+        return`${this.name} is drawn`;
+    }
+
+    calculateArea(length,width){
+        return 2*(length*width);
+        
+    }
+    color(color){
+        return `${color} is Applied on ${this.name}`;
+    }
+    
 }
-
-// export class using module.exports
 module.exports = Shape;
+// export{Shape} from 'shape.js' ;
+// export class using module.exports
